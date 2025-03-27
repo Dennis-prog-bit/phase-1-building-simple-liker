da9100d150d1170eaebe6a9f16e7657eb94c6207
Dennis Kyalo <kyalodennis1738@gmail.com>
2025-03-27 12:54:54 +0300

    final

--- main.js
+++ main.js
@@ -3,6 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph'); 
+const modal = document.querySelector('#modal');
+const modalMessage = document.querySelector('#modal-message');
+
+modal.classList.add('hidden');
+
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+      .then(() => {
+        if (heart.textContent === EMPTY_HEART) {
+          handleHeartSuccess(heart);
+        } else {
+          handleHeartUnsuccess(heart);
+        }
+      })
+      .catch((error) => {
+        handleHeartFailure(error);
+      });
+  });
+});
+
+function handleHeartSuccess(heart) {
+  heart.textContent = FULL_HEART;
+  heart.classList.add('activated-heart');
+}
+
+function handleHeartUnsuccess(heart) {
+  heart.textContent = EMPTY_HEART;
+  heart.classList.remove('activated-heart');
+}
+
+function handleHeartFailure(error) {
+  modalMessage.textContent = error;
+  modal.classList.remove('hidden');
+  
+  setTimeout(() => {
+    modal.classList.add('hidden');
+  }, 3000);
+}
 
 
 
